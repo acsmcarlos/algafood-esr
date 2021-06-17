8d29e22f61da33644269597a91115a31bc22e985
acsmcarlos <acsmcarlos@gmail.com>
2021-06-17 11:05:14 -0300

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/antonio/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/antonio/algafood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
-	MENSAGEM_INCOPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível!"),
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível!"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada!"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso!"),
 	ERRO_NEGOCIO_EXCEPTION("/erro-negocio-exception", "Violação de regra de negócio!");
